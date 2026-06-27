c8bfe9e73a42c0cda7c34961f573a5b4a8549bbf
BaldursGoonsack <149293305+BaldursGoonsack@users.noreply.github.com>
2026-06-27 22:56:16 +1000

    WIP SE table for gear technical statuses in templates

---
+++ Goon's Library/Mods/Goon's_Library/ScriptExtender/Lua/Server/_GearTechnicalApplication.lua
@@ -0,0 +1,32 @@
+-- Ye ye
+
+Goon_Armour_BootsClothing_StatusMap = {
+    ["bc090f4e-ff74-49f6-a3f2-7eb561f57436"] = "GOON_BOOTSOFAIDANDCOMFORT_ARMOUR_TECHNICAL",        -- Boots of Aid and Comfort
+    ["3908725d-29d1-4c9a-be46-4e03c8c65238"] = "GOON_BOOTSOFSTORMYCLAMOUR_ARMOUR_TECHNICAL",        -- Boots of Stormy Clamour
+    ["480a8519-e375-4ce0-ae0a-513bc9d39820"] = "GOON_BOOTSOFVERYFASTBLINKING_ARMOUR_TECHNICAL",     -- Boots of Very Fast Blinking
+    ["eff1515c-4612-4e23-9b50-71f318a2f117"] = "GOON_CINDERSHOES_ARMOUR_TECHNICAL",                 -- Cinder Shoes
+    ["6ca10a61-07d8-478c-a6e7-8e43072233cf"] = "GOON_HOARFROSTBOOTS_ARMOUR_TECHNICAL",              -- Hoarfrost Boots
+    ["baae9783-dcd3-4d6b-bba3-86faf2fe4c8e"] = "GOON_MYSTRASGRACE_ARMOUR_TECHNICAL",                -- Mystra's Grace
+    ["d0666101-d24e-4d2b-bc63-63f67532b38c"] = "GOON_SPIDERSTEPBOOTS_ARMOUR_TECHNICAL",             -- Spiderstep Boots
+    ["48408219-2ab9-4f08-8cb8-2d955fb3dae8"] = "GOON_TYRANNICALJACKBOOTS_ARMOUR_TECHNICAL",         -- Tyrannical Jackboots
+    ["3462aa18-bb93-403c-b28d-9841c147787f"] = "GOON_VARSHKOKUUSBOOTS_ARMOUR_TECHNICAL",            -- Varsh Ko'kuu's Boots
+    ["32b1a84f-2e32-4249-8b19-ecaa7f279cb0"] = "GOON_ACROBATSHOES_ARMOUR_TECHNICAL",                -- Acrobat Shoes
+    ["8838eb53-ed3d-4d64-9fe7-a82ab7249c18"] = "GOON_BOOTSOFAPPARENTDEATH_ARMOUR_TECHNICAL",        -- Boots of Apparent Death
+    ["400b3aca-fc9e-44c7-b5a7-a183693d3a75"] = "GOON_BOOTSOFARCANEBOLSTERING_ARMOUR_TECHNICAL",     -- Boots of Arcane Bolstering
+    ["a7ce200c-ab1a-4d49-bf7d-c6894b1e5b06"] = "GOON_BOOTSOFBRILLIANCE_ARMOUR_TECHNICAL",           -- Boots of Brilliance
+    ["0624ccf0-1b77-448f-8da9-d8a04efa4935"] = "GOON_BOOTSOFGENIALSTRIDING_ARMOUR_TECHNICAL",       -- Boots of Genial Striding
+    ["8b22d15a-85bb-4c8d-90cf-a773fc451eac"] = "GOON_BOOTSOFSPEED_ARMOUR_TECHNICAL",                -- Boots of Speed
+    ["1cd85fa0-b46a-43c6-88d2-e90ad4e73cf7"] = "GOON_BOOTSOFUNINHIBITEDKUSHIGO_ARMOUR_TECHNICAL",   -- Boots of Uninhibited Kushigo
+    ["119f753a-8b06-44d8-bb9a-8a307f6032da"] = "GOON_EVASIVESHOES_ARMOUR_TECHNICAL",                -- Evasive Shoes
+    ["edb7385a-e4d4-4fb7-ad9f-a9910e4b9e97"] = "GOON_LINEBREAKERBOOTS_ARMOUR_TECHNICAL",            -- Linebreaker Boots
+    ["9b94fd74-bcee-4d30-8862-0a18ae65541b"] = "GOON_EVASIVESHOES_ARMOUR_TECHNICAL",                -- Slinging Shoes
+    ["ae4af670-5674-4fed-bbf9-4083f06519b0"] = "GOON_SPACESHUNTBOOTS_ARMOUR_TECHNICAL",             -- Spaceshunt Boots
+    ["1a6825cb-95d5-4542-b018-deb48d936b73"] = "GOON_SPRINGSTEPBOOTS_ARMOUR_TECHNICAL",             -- Springstep Boots
+    ["b9872a9c-d653-4108-9060-21d6d8ec511a"] = "GOON_SWIRESYSHOES_ARMOUR_TECHNICAL",                -- Swiresy Shoes
+    ["fc9104ab-b61c-4481-8402-cecb13baf30f"] = "GOON_THEWATERSPARKERS_ARMOUR_TECHNICAL",            -- The Watersparkers
+    ["23806fd3-f5f6-4ff3-826d-054afe03b7c0"] = "GOON_BONESPIKEBOOTS_ARMOUR_TECHNICAL",              -- Bonespike Boots
+    ["bc82f909-ade5-4ada-9b94-cec7ca1d4a68"] = "GOON_HELLDUSKBOOTS_ARMOUR_TECHNICAL",               -- Helldusk Boots
+    ["ac9145d1-31d0-4aa3-8755-62cc85dad22b"] = "GOON_DISINTEGRATINGNIGHTWALKERS_ARMOUR_TECHNICAL",  -- Disintegrating Night Walkers
+    ["93cc1480-9fe1-4e76-8c0b-0ad3caf35285"] = "GOON_BRISKWINDBOOTS_ARMOUR_TECHNICAL",              -- Briskwind Boots (Innaccessible/cut content)
+    ["6cae9a98-b75d-408e-ac59-d6bde6e4ab13"] = "GOON_SHADOWSTEPBOOTS_ARMOUR_TECHNICAL",             -- Shadowstep Boots (Innaccessible/cut content)
+}
